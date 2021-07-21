cf1f58c4f734440cfe096f28e667c2b8f980cb9c
maxim <maximum454@gmail.com>
2021-07-21 17:32:14 +0600

    slider fix

--- src/js/hair.js
+++ src/js/hair.js
@@ -1,3 +1,22 @@
 $(function () {
+    const swiper = new Swiper('.video-slider', {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        centeredSlidesBounds: false,
+        slidesPerView: 2,
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 10,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
+        },
 
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 })
