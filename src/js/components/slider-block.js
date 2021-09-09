d691f0e02f4a3f99a4ad4e3ec848409d43a8a425
Maxim Karachyov <karachyov.m@gmail.com>
2021-09-09 15:13:06 +0600

    ryzhiy убрал слайдер для мобильных устройств

--- src/js/components/slider-block.js
+++ src/js/components/slider-block.js
@@ -5,7 +5,9 @@ var swiperDiscountInit = false;
 function swiperDiscountMode() {
     let mobile = window.matchMedia('(min-width: 0px) and (max-width: 767px)');
     let desktop = window.matchMedia('(min-width: 768px)');
+    let ryzhiy = document.querySelector('.wrp-ryzhiy');
     let sliderSpaceBetween = document.querySelector('.swiper-container-discount').getAttribute('data-spaceBetween');
+    console.log(ryzhiy)
     if (desktop.matches) {
         if(swiperDiscount){
             swiperDiscount.destroy();
@@ -13,6 +15,12 @@ function swiperDiscountMode() {
         }
 
 
+    }
+    else if (ryzhiy){
+        if(swiperDiscount){
+            swiperDiscount.destroy();
+            swiperDiscountInit = false;
+        }
     }
 
     else if (mobile.matches) {
